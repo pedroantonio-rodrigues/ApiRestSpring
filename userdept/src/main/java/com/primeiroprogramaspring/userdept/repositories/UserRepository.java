829382faeeb1a5491a03d29d454f7f470f23888e
Pedro Antonio Rodrigues <pedro_arxp@hotmail.com>
2022-10-30 12:30:09 -0300

    Add files via upload

---
+++ userdept/src/main/java/com/primeiroprogramaspring/userdept/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.primeiroprogramaspring.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.primeiroprogramaspring.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
